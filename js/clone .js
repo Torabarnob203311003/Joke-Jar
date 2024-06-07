8b5639b7d3019f2503b8589a99ab19e2790ad76b
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-07 21:58:02 +0600

    description are added

---
+++ js/clone .js
@@ -0,0 +1,25 @@
+// these are the clone code of index.js so ignore it 
+
+const loadJoke = () =>{
+
+  fetch('https://official-joke-api.appspot.com/jokes/random')
+    .then(response => response.json())
+    .then(data => DisplayJoke (data));
+    
+ data =''
+}
+// loadJoke()
+
+const DisplayJoke = jokes=>{
+  const Textarea=document.getElementById('text-area')
+   
+  console.log(jokes);
+//   Textarea.innerText = ''
+  Textarea.innerText = jokes.setup;
+  Textarea.append(document.createTextNode("\n \n🤪 ")); // Append a newline character
+  Textarea.append(jokes.punchline);
+//   document.getElementById('text-ares').innerHTML = jokes.punchline;
+
+
+
+}
